dfc4bccf1a68fe77a736a1bafad310583d370c33
dschloe <j2hoon85@gmail.com>
2022-05-10 16:28:52 +0900

    updated

--- my_app/todo_list/views.py
+++ my_app/todo_list/views.py
@@ -1,9 +1,12 @@
 from wsgiref.util import request_uri
 from django.shortcuts import render
+from .models import List
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    all_items = List.objects.all
+    return render(request, 'home.html', {'all_items' : all_items})
 
 def about(request):
-    return render(request, 'about.html', {})
+    context = {'first_name': 'Evan', 'last_name' : 'Elder'}
+    return render(request, 'about.html', context)
